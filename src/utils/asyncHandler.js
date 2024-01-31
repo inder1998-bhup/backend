f5090f12120142059433a63e3388968f4cab5543
inder1998-bhup <inder.is147@gmail.com>
2024-01-31 14:31:52 +0530

    utils

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 import { Promise } from "mongoose"
 
 const asyncHandler = (requestHandler)=>{
-    (req,res,next) =>{
+    return (req,res,next) =>{
         Promise.resolve(requestHandler(res,req,next)).catch((err)=> next(err));
     }
 }
