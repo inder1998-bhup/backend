af14b64dd466da2e881591a1456599cd0a54b04a
inder1998-bhup <inder.is147@gmail.com>
2024-02-03 11:19:12 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,20 +1,12 @@
-import { Promise } from "mongoose"
 
-const asyncHandler = (requestHandler)=>{
-    return (req,res,next) =>{
-        Promise.resolve(requestHandler(res,req,next)).catch((err)=> next(err));
-    }
-}
-
-export { asyncHandler }
-
-// const asyncHandler = (fn) => async (req, res, next)=>{
-//     try {
-//         await fn(req, res, next);
-//     } catch (error) {
-//         res.status(error.code || 500).json({
-//             success: true,
-//             msg: error.message
-//         })
-//     }
-// }
+const asyncHandler = (fn) => async (req, res, next)=>{
+        try {
+                await fn(req, res, next);
+            } catch (error) {
+                    res.status(error.code || 500).json({
+                            success: true,
+                            msg: error.message
+                        })
+                    }
+                }
+export default asyncHandler
