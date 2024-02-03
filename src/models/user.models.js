ca65b21402e87dab98d503b6c00addcfaa29fb72
inder1998-bhup <inder.is147@gmail.com>
2024-02-03 08:03:09 +0530

    jwt setup

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -62,4 +62,31 @@ userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id, 
+        },
+        process.env.REFRESH_TOKEN,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES
+        }
+    )
+}
+
 export const User = mongoose.model("User", userSchema);
